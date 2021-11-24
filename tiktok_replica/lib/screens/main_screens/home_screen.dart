0c1983c7b658671677af32b305c19517f60db891
DrinKrasniqiNovus <90898549+DrinKrasniqiNovus@users.noreply.github.com>
2021-11-24 14:43:03 +0100

    Update home_screen.dart

--- tiktok_replica/lib/screens/main_screens/home_screen.dart
+++ tiktok_replica/lib/screens/main_screens/home_screen.dart
@@ -1,13 +1,7 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:tiktok_replica/screens/video_post.dart';
 
-import 'package:tiktok_replica/screens/getters/username.dart';
-import 'package:tiktoklikescroller/tiktoklikescroller.dart';
-import 'package:video_player/video_player.dart';
-
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   _HomeScreenState createState() => _HomeScreenState();
